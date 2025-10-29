42a89aa805b8b6efbff1e9dce382997be5af9810
SHull156 <sarahthull@gmail.com>
2025-10-29 18:15:19 +0000

    Added weather fetch and display functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+let inputBox = document.getElementById("cityInput");
+let searchBtn = document.getElementById("searchBtn");
+let resultContainer = document.getElementById("weatherResult"); 
+
+searchBtn.addEventListener("click", function(){
+    let city = inputBox.value;
+    let apiKey = "8a7c562feb5f86348243418982710185";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then (data => {
+            resultContainer.innerHTML = `
+            <p>Temperature: ${data.main.temp} °C </p>
+            <p> Description: ${data.weather[0].description}</p>
+            <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+            `;
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+            resultContainer.innerHTML = "<p>Could not fetch data. Try again.</p>";
+        });
+});
